dbbe91ee60f73b42757dc229530add947cc846a1
Excapts <andrewshiryaev05@gmail.com>
2021-04-24 19:01:58 +0700

    twetwoap

--- conf.py
+++ conf.py
@@ -1,2 +1,2 @@
 # Токен бота
-bot_token = "ODI3ODQ5MDY1MDgwODE1NjE2.YGhAhQ.pS6FYY9RypKvtTAHIXP1ntwp_vU"
+bot_token = "ODI3ODQ5MDY1MDgwODE1NjE2.YGhAhQ.8J2MU2FoBBmX0ial-Z4GoSe_slg"
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # Импортируем найстройки бота
+import os
 import conf
 import discord
+import img_headler as imh1
 from discord.ext import commands
 
 
 
 
-
-# intents = discord.Intents.default()
-# intents.members = True
+intents = discord.Intents.default()
+intents.members = True
 # # Создаём подключение бота
 # client = discord.Client(intents = intents)
 
@@ -24,17 +25,17 @@ from discord.ext import commands
 
 
 
-#     #Защита от дурока №1: Защита от рекурсии
-#     if message.author == client.user:
-#         return
-#     #Защита от дурока №2: Защита от ответа бота боту
-#     if message.author.bot:
-#         return
+    # #Защита от дурока №1: Защита от рекурсии
+    # if message.author == client.user:
+    #     return
+    # #Защита от дурока №2: Защита от ответа бота боту
+    # if message.author.bot:
+    #     return
 
 
-#     #Бот отвечает только в комнате "Andrew-bot"
-#     if message.channel.id == 827854405511544853: 
-#         msg = None
+    # #Бот отвечает только в комнате "Andrew-bot"
+    # if message.channel.id == 827854405511544853: 
+    #     msg = None
 
 #         ctx = message.content.split(" ", maxsplit = 1)
 
@@ -69,8 +70,8 @@ from discord.ext import commands
 #         # if ctx == "/get_channels":
 #         #     msg = ""
 #         #     if message.author.guild.name == "Bots":
-#         #         for idx, channel in list(enumerate(message.author.guild.channels)):   Не робит
-#         #             msg += f'{idx+1}. {channel.name} - {channle.id}\n'
+#         #         for idx, channel in list(enumerate(message.author.guild.channels)):   
+#         #             msg += f'{idx+1}. {channel.name} - {channel.id}\n'
         
 #         #Защита от дурака №3: Проверка на пустоту
 #         if msg:
@@ -82,14 +83,119 @@ from discord.ext import commands
 # client.run(conf.bot_token)
 
 
-bot = commands.Bot(command_prefix = "!")
+bot = commands.Bot(command_prefix = "!", intents=intents)
 
+
+
+
+#Ответный привет
 @bot.command(name = "hello")
 async def command_hello(ctx, *args):
 
     if ctx.channel.id == 827854405511544853: 
         msg = "Hello you!"
         await ctx.channel.send(msg)
+
+#Обо мне
+@bot.command(name = "about_me")
+async def command_about_me(ctx, *args):
+    if ctx.channel.id == 827854405511544853: 
+        msg = f'Здравствуйте, ваш id: "{ctx.author.id}", ваше имя: "{ctx.author.name}", ваш ник: "{ctx.author.nick}"'
+        await ctx.channel.send(msg)
+
+#Повтор слова/слов
+@bot.command(name = "repeat")
+async def command_repeat(ctx, *args):
+    if ctx.channel.id == 827854405511544853: 
+        msg = " ".join(args)
+        await ctx.channel.send(msg)
+
+
+#Список всех каналов
+@bot.command(name = "get_channels")
+async def command_get_channels(ctx, *args):
+    if ctx.channel.id == 827854405511544853: 
+        msg = " "
+        if ctx.author.guild.name == "Bots":
+            for idx, channel in list(enumerate(ctx.author.guild.channels)):  
+                msg += f'{idx+1}. {channel.name} - {channel.id}\n'
+        await ctx.channel.send(msg)
+
+
+
+#Список всех участников
+@bot.command(name = "get_members")
+async def command_get_members(ctx, *args):
+    if ctx.channel.id == 827854405511544853: 
+        if ctx.author.guild.name == "Bots":
+            msg=""
+            for idx, member in list(enumerate(ctx.author.guild.members)):
+                msg += f'{idx+1}. {member.name}, {member.nick} - {member.id}\n'
+        await ctx.channel.send(msg)
+
+channel = 827854405511544853
+
+@bot.command(name="get_member")
+async def get_member(ctx, member:discord.Member=None):
+    msg = None
+    global channel
+    if ctx.channel.id == channel:
+
+        if member:
+            msg = f'Member: {member.name}, Nickname: {member.nick} - {member.id}'
+
+        if msg == None:
+            msg = "error"
+        
+        await ctx.channel.send(msg)
+
+
+
+@bot.command(name="mk")
+async def mk (ctx, fighter1:discord.Member=None, fighter2:discord.Member=None):
+    global channel
+    msg = None
+    if ctx.channel.id == channel:
+        if fighter1 and fighter2:
+            await imh1.vs_create(fighter1.avatar_url, fighter2.avatar_url)
+
+
+            msg = f'{fighter1.name} VS {fighter2.name}'
+            await ctx.channel.send(file=discord.File(os.path.join("./img/result.png")))
+
+
+@bot.command(name="mka")
+async def mka (ctx, fighter1:discord.Member=None, fighter2:discord.Member=None):
+    global channel
+    if ctx.channel.id == channel:
+        if fighter1 and fighter2:
+            await imh1.vs_create_animated(fighter1.avatar_url, fighter2.avatar_url)
+
+            await ctx.channel.send(file=discord.File(os.path.join("./img/result.gif")))
+
+
+@bot.command(name="join")
+async def vc_join(ctx):
+    msg =""
+    global channel
+    voice_channel = ctx.author.voice.channel
+
+    if voice_channel:
+        msg = f'Подключаюсь к {voice_channel.name}'
+        await ctx.channel.send(msg)
+        await voice_channel.connect()
+
+@bot.command(name="leave")
+async def vc_leave(ctx):
+    msg =""
+    global channel
+    voice_channel = ctx.author.voice.channel
+
+    if voice_channel:
+        msg = f'Отключаюсь от {voice_channel.name}'
+        await ctx.channel.send(msg)
+        await ctx.voice_client.disconnect()
+
 
 
 bot.run(conf.bot_token)
